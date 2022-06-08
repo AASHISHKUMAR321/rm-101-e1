bb1353c3322e1bf0ffb0c4aa758eaaa6884a719e
AASHISHKUMAR321 <you@example.com>
2022-06-08 18:56:58 +0530

    aashish

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { validate } from "uuid";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
+  const [task, setTask] = useState("");
+  const inputHandler = (e) => {
+    setTask(e.target.value);
+  };
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+      <input
+        data-testid="add-task-input"
+        type="text"
+        placeholder="Add Task..."
+        onChange={inputHandler}
+      />
+      <button
+        data-testid="add-task-button"
+        onClick={() => {
+          console.log(task);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 };
--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -3,14 +3,17 @@ import styles from "./taskHeader.module.css";
 
 const TaskHeader = () => {
   // sample values to be replaced
-  let totalTask = 0;
-  let unCompletedTask = 0;
+  let totalTask = 5;
+  let unCompletedTask = 3;
 
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div data-testid="task-header" className={styles.taskHeader}>
+      <span>You Have a </span>{" "}
       <b data-testid="header-remaining-task">{unCompletedTask}</b>
+      <span>of</span>
       <b data-testid="header-total-task">{totalTask}</b>
+      <span>Tasks Remaining</span>
     </div>
   );
 };
